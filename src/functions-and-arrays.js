40eedc8368b0875f0eda0edc873643234588b4c6
Robert Muresan <robotzelu89@gmail.com>
2025-01-16 17:16:10 +0000

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,16 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+//Implement the function maxOfTwoNumbers that takes two numbers as arguments and returns the bigger number.
+function maxOfTwoNumbers(a, b) {
+    if (a > b) {
+        return a;
+    } else if (a < b) {
+        return b;
+    }else {
+        return a;
+    }
+
+    //return Math.max(a, b);
+}
 
 
 
@@ -7,7 +18,18 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    if (words.length === 0) {
+      return null;
+    }
+    let longestWord = words[0];
+    for (let i = 1; i < words.length; i++) {
+      if (words[i].length > longestWord.length) {
+        longestWord = words[i];
+      }
+    }
+    return longestWord;
+  }
 
 
 
@@ -15,7 +37,21 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    if (numbers.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+
+    /*let sum = 0;
+    numbers.forEach(function (number) {
+        sum += number;
+    });*/
+}
 
 
 
@@ -23,7 +59,16 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+    if (numbers2.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < numbers2.length; i++) {
+        sum += numbers2[i];
+    }
+    return sum / numbers2.length;
+}
 
 
 
@@ -31,4 +76,16 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words2, word) {
+    if (words2.length === 0) {
+        return null;
+    }
+    
+    for (let i = 0; i < words2.length; i++) {
+        if (words2[i] === word) {
+            return true;
+        }
+    }
+    return false;
+}
+
